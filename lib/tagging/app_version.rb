a948c1aea8898116bb5936179112b919f410a5d3
Altherlex Alves <para.alves@gmail.com>
2016-04-28 12:43:31 -0300

    new rake next_version

--- lib/tagging/app_version.rb
+++ lib/tagging/app_version.rb
@@ -5,14 +5,14 @@ module Tagging
     @@default_commit_message = 'versioning by CI'
     @@version
 
-    def self.next_version
+    def self.version
       @@version ||= `git -C #{Dir.pwd} describe --tags $(git -C #{Dir.pwd} for-each-ref refs/tags --sort=-taggerdate --format='%(objectname)' --count=1)`.chomp
       @@version = self.init_version_file unless self.version_format_valid?(@@version)
       @@version
     end
 
-    def self.version_format_valid?(version)
-      (version && version =~ /^v\d+\.\d+\.\d+-[\.\d+]+$/)
+    def self.version_format_valid?(v)
+      (v && v =~ /^v\d+\.\d+\.\d+-[\.\d+]+$/)
     end
 
     def self.init_version_file
@@ -25,13 +25,13 @@ module Tagging
     end
 
     def self.increment_version
-      self.next_version
+      @@version = self.version.next
     end
 
     def self.commit_and_push(project_directory = nil, msg = nil, &block)
       g=Git.open(project_directory || Dir.pwd, :log=>Logger.new(STDOUT))
-      g.add_tag(self.next_version, nil, message: @@default_commit_message, f: true)
-      g.push('origin', "refs/tags/#{self.next_version}", f: true)
+      g.add_tag(self.version, nil, message: @@default_commit_message)
+      g.push('origin', "refs/tags/#{self.version}", f: true)
     end
   end
 end
--- lib/tagging/tasks/app_version.rake
+++ lib/tagging/tasks/app_version.rake
@@ -9,4 +9,14 @@ namespace :tagging do
   task :version do
     puts Tagging::AppVersion.next_version
   end
+
+  desc "Setting next gem version and push the tag version"
+  task :version do
+    puts Tagging::AppVersion.version
+  end
+
+  desc "Setting next gem version and push the tag version"
+  task :next_version do
+    puts Tagging::AppVersion.increment_version
+  end
 end
